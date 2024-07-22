abaf95d26cd25e1664d4378edc7ffea2d7edd737
romulofull <romulo.castro@gmail.com>
2024-07-21 22:30:34 -0500

    Arraylist

--- src/main/java/com/clientes/customers/controllers/CustomerController.java
+++ src/main/java/com/clientes/customers/controllers/CustomerController.java
@@ -1,13 +1,26 @@
 package com.clientes.customers.controllers;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CustomerController {
     @GetMapping("/prueba")
     public String prueba() {
-return "Hola Mundo";
+return "Hola Mundo"; }
+
+
+
+@GetMapping("/prueba2")
+
+        public List<String> prueba2() {
+             List<String> list =  new ArrayList<>();
+            list.add ("Lucas");
+            list.add ("Romulo");
+            list.add ( "Luis");
+            return list;
     }
 }
